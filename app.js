cb4aca80da9f6c328088510ffee7d05dbe08c647
Your Name <your_email@gmail.com>
2026-07-14 12:08:16 +0530

    Prepare project for deployment

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 // Set EJS as the template engine
 app.set("view engine", "ejs");
@@ -44,5 +44,5 @@ app.post("/submit", (req, res) => {
 
 // Start the server
 app.listen(PORT, () => {
-    console.log(`Server is running at http://localhost:${PORT}`);
+    console.log(`Server is running at ${PORT}`);
 });
